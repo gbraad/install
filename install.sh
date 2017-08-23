5df38e2629633b4f086bf3bb122e538ce08bec31
Nirmal Almara <hello@nirm.al>
2017-08-23 06:28:19 +0530

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+apt-get install -y redis-server
+
+apt-get install -y unzip
+
 wget -qO- https://get.docker.com/ | sh
 
 curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
@@ -8,4 +12,6 @@ chmod +x /usr/local/bin/docker-compose
 
 sysctl -w vm.max_map_count=262144
 
-docker-compose up
+docker-compose up -d
+
+./server
