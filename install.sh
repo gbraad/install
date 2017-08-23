bc303fbd36796e073ac5f061aac903c20153b887
Nirmal Almara <hello@nirm.al>
2017-08-23 06:09:03 +0530

    update setup

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 wget -qO- https://get.docker.com/ | sh
+
+curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+sysctl -w vm.max_map_count=262144
+
+docker-compose up
