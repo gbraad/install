50d56702348e9289924de73f6c7a1835b981ef3c
mysticmode <hello@nirm.al>
2017-12-11 19:38:16 +0530

    bug fix

--- docker_es.sh
+++ docker_es.sh
@@ -8,7 +8,7 @@ sysctl -w vm.max_map_count=262144
 
 docker build -t my_es .
 
-docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" --name my_es_instance -i -t my_es
+docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" --name my_es_instance -i -t my_es
 
 sleep 20
 
