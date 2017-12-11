435615dd79655bf0d16e1cac9c2bb75e09f7ffd7
mysticmode <hello@nirm.al>
2017-12-11 19:43:19 +0530

    sleep 30 for es docker to ready

--- docker_es.sh
+++ docker_es.sh
@@ -10,7 +10,7 @@ docker build -t my_es .
 
 docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" --name my_es_instance -i -t my_es
 
-sleep 20
+sleep 30
 
 ./es_setup
 
