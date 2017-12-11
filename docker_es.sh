e8ba66aacc08db8e5ee7302144a502ddef3468c6
mysticmode <hello@nirm.al>
2017-12-11 19:16:22 +0530

    docker_es update

--- docker_es.sh
+++ docker_es.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+systemctl stop libreread
+
 wget -qO- https://get.docker.com/ | sh
 
 sysctl -w vm.max_map_count=262144
@@ -8,4 +10,8 @@ docker build -t my_es .
 
 docker run -d -p 9200:9200 -p 9300:9300 --name my_es_instance -i -t my_es
 
+sleep 20
+
 ./es_setup
+
+systemctl start libreread
